e1a1469fca62ef5d6e8869951eb3ba4a212a1668
Thiago da Fonte <thiagofb.dqd@gmail.com>
2023-03-23 17:04:55 -0300

    a TOML for each necessity, starting with config

--- RollbackShooter/Config.hpp
+++ RollbackShooter/Config.hpp
@@ -53,79 +53,79 @@ struct Config
 Config readTOMLForCfg()
 {
 	Config cfg;
-	auto file = toml::parse_file("RBST.toml");
+	auto file = toml::parse_file("RBST_config.toml");
 	std::istringstream iss;
 	num_det extract;
 	num_det sixty{ 60 };
 	num_det thirtySixHundred{ 3600 };
 
-	cfg.playerHealth = file["GameConfig"]["GameRules"]["playerHealth"].value_or(5);
-	cfg.roundsToWin = file["GameConfig"]["GameRules"]["roundsToWin"].value_or(2);
+	cfg.playerHealth = file["GameRules"]["playerHealth"].value_or(5);
+	cfg.roundsToWin = file["GameRules"]["roundsToWin"].value_or(2);
 
-	cfg.ammoMax = file["GameConfig"]["PlayerResources"]["ammoMax"].value_or(120);
-	cfg.shotCost = file["GameConfig"]["PlayerResources"]["shotCost"].value_or(60);
-	cfg.altShotCost = file["GameConfig"]["PlayerResources"]["altShotCost"].value_or(120);
-	cfg.staminaMax = file["GameConfig"]["PlayerResources"]["staminaMax"].value_or(150);
-	cfg.dashCost = file["GameConfig"]["PlayerResources"]["dashCost"].value_or(50);
+	cfg.ammoMax = file["PlayerResources"]["ammoMax"].value_or(120);
+	cfg.shotCost = file["PlayerResources"]["shotCost"].value_or(60);
+	cfg.altShotCost = file["PlayerResources"]["altShotCost"].value_or(120);
+	cfg.staminaMax = file["PlayerResources"]["staminaMax"].value_or(150);
+	cfg.dashCost = file["PlayerResources"]["dashCost"].value_or(50);
 
-	cfg.dashPhase = file["GameConfig"]["PlayerActions"]["dashPhase"].value_or(10);
-	cfg.dashPerfect = file["GameConfig"]["PlayerActions"]["dashPerfect"].value_or(15);
-	cfg.dashDuration = file["GameConfig"]["PlayerActions"]["dashDuration"].value_or(60);
-	cfg.chargeDuration = file["GameConfig"]["PlayerActions"]["chargeDuration"].value_or(60);
+	cfg.dashPhase = file["PlayerActions"]["dashPhase"].value_or(10);
+	cfg.dashPerfect = file["PlayerActions"]["dashPerfect"].value_or(15);
+	cfg.dashDuration = file["PlayerActions"]["dashDuration"].value_or(60);
+	cfg.chargeDuration = file["PlayerActions"]["chargeDuration"].value_or(60);
 
-	iss.str(file["GameConfig"]["PlayerSpeed"]["playerWalkSpeed"].value_or("0.8"));
+	iss.str(file["PlayerSpeed"]["playerWalkSpeed"].value_or("0.8"));
 	iss >> extract;
 	cfg.playerWalkSpeed = extract / sixty;
-	iss.str(file["GameConfig"]["PlayerSpeed"]["playerWalkAccel"].value_or("0.4"));
+	iss.str(file["PlayerSpeed"]["playerWalkAccel"].value_or("0.4"));
 	iss >> extract;
 	cfg.playerWalkAccel = extract / thirtySixHundred;
-	iss.str(file["GameConfig"]["PlayerSpeed"]["playerWalkFric"].value_or("0.4"));
+	iss.str(file["PlayerSpeed"]["playerWalkFric"].value_or("0.4"));
 	iss >> extract;
 	cfg.playerWalkFric = extract / thirtySixHundred;
-	iss.str(file["GameConfig"]["PlayerSpeed"]["playerDashSpeed"].value_or("1.2"));
+	iss.str(file["PlayerSpeed"]["playerDashSpeed"].value_or("1.2"));
 	iss >> extract;
 	cfg.playerDashSpeed = extract / sixty;
 
-	iss.str(file["GameConfig"]["ProjSpeed"]["projSpeed"].value_or("0.6"));
+	iss.str(file["ProjSpeed"]["projSpeed"].value_or("0.6"));
 	iss >> extract;
 	cfg.playerWalkSpeed = extract / sixty;
-	iss.str(file["GameConfig"]["ProjSpeed"]["projCounterMultiply"].value_or("1.2"));
+	iss.str(file["ProjSpeed"]["projCounterMultiply"].value_or("1.2"));
 	iss >> extract;
 	cfg.playerWalkSpeed = extract;
 
-	iss.str(file["GameConfig"]["Radius"]["playerRadius"].value_or("0.5"));
+	iss.str(file["Radius"]["playerRadius"].value_or("0.5"));
 	iss >> extract;
 	cfg.playerRadius = extract;
-	iss.str(file["GameConfig"]["Radius"]["grazeRadius"].value_or("0.7"));
+	iss.str(file["Radius"]["grazeRadius"].value_or("0.7"));
 	iss >> extract;
 	cfg.grazeRadius = extract;
-	iss.str(file["GameConfig"]["Radius"]["projRadius"].value_or("0.3"));
+	iss.str(file["Radius"]["projRadius"].value_or("0.3"));
 	iss >> extract;
 	cfg.projRadius = extract;
-	iss.str(file["GameConfig"]["Radius"]["comboRadius"].value_or("3.0"));
+	iss.str(file["Radius"]["comboRadius"].value_or("3.0"));
 	iss >> extract;
 	cfg.comboRadius = extract;
-	iss.str(file["GameConfig"]["Radius"]["arenaRadius"].value_or("12.0"));
+	iss.str(file["Radius"]["arenaRadius"].value_or("12.0"));
 	iss >> extract;
 	cfg.arenaRadius = extract;
-	iss.str(file["GameConfig"]["Radius"]["spawnRadius"].value_or("10.0"));
+	iss.str(file["Radius"]["spawnRadius"].value_or("10.0"));
 	iss >> extract;
 	cfg.spawnRadius = extract;
 
-	iss.str(file["GameConfig"]["HitStrength"]["weakForce"].value_or("1.0"));
+	iss.str(file["HitStrength"]["weakForce"].value_or("1.0"));
 	iss >> extract;
 	cfg.weakForce = extract / sixty;
-	cfg.weakHitstop = file["GameConfig"]["HitStrength"]["weakHitstop"].value_or(5);
+	cfg.weakHitstop = file["HitStrength"]["weakHitstop"].value_or(5);
 
-	iss.str(file["GameConfig"]["HitStrength"]["midForce"].value_or("2.5"));
+	iss.str(file["HitStrength"]["midForce"].value_or("2.5"));
 	iss >> extract;
 	cfg.midForce = extract / sixty;
-	cfg.midHitstop = file["GameConfig"]["HitStrength"]["midHitstop"].value_or(10);
+	cfg.midHitstop = file["HitStrength"]["midHitstop"].value_or(10);
 
-	iss.str(file["GameConfig"]["HitStrength"]["strongForce"].value_or("4.0"));
+	iss.str(file["HitStrength"]["strongForce"].value_or("4.0"));
 	iss >> extract;
 	cfg.strongForce = extract / sixty;
-	cfg.strongHitstop = file["GameConfig"]["HitStrength"]["strongHitstop"].value_or(20);
+	cfg.strongHitstop = file["HitStrength"]["strongHitstop"].value_or(20);
 
 	return cfg;
 }
